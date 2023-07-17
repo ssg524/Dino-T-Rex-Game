289e82ad760444bef7ca07bd262310910fa91d76
GiSeok Kang <ime.rokz@gmail.com>
2023-07-18 03:07:43 +0900

    [2] - 적
    적 Prefab, 충돌 범위, 생성 등 정의

--- Pixel Runner/Assets/Scripts/EnemySpawner.cs
+++ Pixel Runner/Assets/Scripts/EnemySpawner.cs
@@ -6,14 +6,37 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] enemies;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartSpawnEnemy();
     }
+
+    void StartSpawnEnemy()
+    {
+        StartCoroutine("SpawnRoutine");
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        while (true)
+        {
+            int index = Random.Range(0, enemies.Length);
+            SpawnEnemy(index);
+
+            float ran = Random.Range(1.0f, 3.0f);
+            yield return new WaitForSeconds(ran);
+        }
+    }
+
 
     void SpawnEnemy(int index)
     {
+        Vector3 pos = new Vector3(transform.position.x, -3.5f, transform.position.z);
+        Instantiate(enemies[index], pos, Quaternion.identity);
     }
 
     // Update is called once per frame
